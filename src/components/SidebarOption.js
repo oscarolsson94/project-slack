65bdddbce27ad652c17a7897db6313520f7a1a1d
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-15 17:46:33 +0200

    adding dispatcher to select room

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { enterRoom } from '../features/appSlice';
 import { db } from '../firebase';
 
-const SidebarOption = ({ Icon, title, addChannelOption }) => {
+const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
+
+    const dispatch = useDispatch();
     
     const addChannel = () => {
         const channelName = prompt('Please enter the channel name');
@@ -15,7 +19,11 @@ const SidebarOption = ({ Icon, title, addChannelOption }) => {
     };
 
     const selectChannel = () => {
-        
+        if (id) {
+            dispatch(enterRoom({
+                roomId: id
+            }))
+        }
     };
 
     return (
