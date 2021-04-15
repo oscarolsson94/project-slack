f7a337ad05adee57f86d94c2236930f5543d1a1f
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-15 16:04:37 +0200

    starting sidebaroption and getting icons for it

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SidebarOption = () => {
+    return (
+        <SidebarOptionContainer>
+
+        </SidebarOptionContainer>
+    )
+}
+
+export default SidebarOption;
+
+const SidebarOptionContainer = styled.div`
+
+`;
