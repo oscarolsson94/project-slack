cf8cdd5883778ede2e147a6fd2a76ff9fec4a28a
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-15 16:51:10 +0200

    setting up firebase and google auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDd9S8ysPRNrmUlSFaLQI9eEXphrGZvRAM",
     authDomain: "slack-clone-5710b.firebaseapp.com",
@@ -5,4 +7,11 @@ const firebaseConfig = {
     storageBucket: "slack-clone-5710b.appspot.com",
     messagingSenderId: "320102293846",
     appId: "1:320102293846:web:2094fe65b98b99444bb6f7"
-  };
+};
+  
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider;
+
+export { auth, provider, db };
