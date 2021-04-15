f216518e05f46573ee2efdeef2564711fd242b59
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-15 13:22:01 +0200

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <h1>lets go</h1>
+          
+          <Router>
+            <>
+                <Switch>
+                    <Route exact path="/">
+                        <h1>Home page</h1>
+                    </Route>
+                </Switch>
+            </>
+          </Router>
+          
     </div>
   );
 }
