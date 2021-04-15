af57e7f50bf00eec10e23efdfba04bdd95af2b01
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-15 15:52:10 +0200

    styling sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
 
 const Sidebar = () => {
     return (
@@ -13,6 +14,7 @@ const Sidebar = () => {
                         Oscar Olsson
                     </h3>
                 </SidebarInfo>
+                <CreateIcon />
             </SidebarHeader>
         </SidebarContainer>
     )
@@ -30,9 +32,39 @@ const SidebarContainer = styled.div`
 `;
 
 const SidebarHeader = styled.div`
+    display: flex;
+    border-bottom: 1px solid #49274b;
+    padding: 13px;
 
+    > .MuiSvgIcon-root {
+        padding: 8px;
+        color: #49274b;
+        font-size: 18px;
+        background-color: white;
+        border-radius: 999px;
+    }
 `;
 
 const SidebarInfo = styled.div`
+    flex: 1;
 
+    > h2 {
+        font-size: 15px;
+        font-weight: 900;
+        margin-bottom: 5px;
+    }
+
+    > h3 {
+        display: flex;
+        font-size: 13px;
+        font-weight: 400;
+        align-items: center;
+    }
+
+    > h3 > .MuiSvgIcon-root{
+        font-size: 14px;
+        margin-top: 1px;
+        margin-right: 2px;
+        color: green;
+    }
 `;
