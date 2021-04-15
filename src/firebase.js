5fdbde34e5c7112804c5ceb38d669a76592fe45a
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-15 12:11:21 +0200

    adding firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDd9S8ysPRNrmUlSFaLQI9eEXphrGZvRAM",
+    authDomain: "slack-clone-5710b.firebaseapp.com",
+    projectId: "slack-clone-5710b",
+    storageBucket: "slack-clone-5710b.appspot.com",
+    messagingSenderId: "320102293846",
+    appId: "1:320102293846:web:2094fe65b98b99444bb6f7"
+  };
