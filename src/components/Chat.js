22ec00e98605e6f8e7df0c08d537033ad420efd0
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-15 18:17:42 +0200

    setting up structure and styling for Chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,12 +3,36 @@ import styled from 'styled-components';
 
 const Chat = () => {
     return <ChatContainer>
-        <h1>chat</h1>
+        <h1>Hello</h1>
+        <Header>
+            <HeaderLeft>
+
+            </HeaderLeft>
+            <HeaderRight>
+                
+            </HeaderRight>
+
+        </Header>
     </ChatContainer>;
 };
 
 export default Chat;
 
-const ChatContainer = styled.div`
+const Header = styled.div`
 
+`;
+
+const HeaderLeft = styled.div`
+
+`;
+
+const HeaderRight = styled.div`
+
+`;
+
+const ChatContainer = styled.div`
+    flex: 0.7;
+    flex-grow: 1;
+    overflow-y: scroll;
+    margin-top: 60px;
 `;
