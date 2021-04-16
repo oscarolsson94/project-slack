732aa3bcfcbd73b7a806de71456afbe628b840a5
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-16 08:17:12 +0200

    adding scroll effect when posting new message

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { db } from '../firebase';
 import firebase from 'firebase';
 
-function ChatInput({ channelName, channelId }) {
+function ChatInput({ channelName, channelId, chatRef }) {
 
     const [input, setInput] = useState('');
     
@@ -20,6 +20,10 @@ function ChatInput({ channelName, channelId }) {
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
             user: "Oscar",
             userImage: ""
+        });
+
+        chatRef.current.scrollIntoView({
+            behavior: "smooth",
         });
 
         setInput("");
