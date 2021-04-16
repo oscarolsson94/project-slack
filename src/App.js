34f589a1aa0089834a091777a8966f1abaa71e7e
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-16 09:58:54 +0200

    styling loading page

--- src/App.js
+++ src/App.js
@@ -65,9 +65,24 @@ const AppBody = styled.div`
 `;
 
 const AppLoading = styled.div`
-
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    width: 100%;
 `;
 
 const AppLoadingContent = styled.div`
 
+    text-align: center;
+    padding-bottom: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    > img {
+        height: 100px;
+        padding: 20px;
+        margin-bottom: 40px;
+    }
 `;
