900de214ca1726151b6b9bf40d9658bc91af0507
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-16 08:01:30 +0200

    setting up message structure

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Message = () => {
+const Message = ({message, timestamp, user, userImage}) => {
     return (
-        <div>
-            
-        </div>
+        <MessageContainer>
+            <img src={userImage} alt="" />
+            <MessageInfo>
+                <h4>
+                    {user}{' '}
+                    <span>{new Date(timestamp?.toDate()).toUTCString()}</span>
+                </h4>
+                <p>{message}</p>
+            </MessageInfo>
+        </MessageContainer>
     )
 }
 
 export default Message;
+
+const MessageContainer = styled.div``;
+
+const MessageInfo = styled.div``;
