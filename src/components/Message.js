81bd591675ba2f9b6eee4fc58dc859913ff3474f
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-16 08:03:54 +0200

    styling message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -18,6 +18,24 @@ const Message = ({message, timestamp, user, userImage}) => {
 
 export default Message;
 
-const MessageContainer = styled.div``;
+const MessageContainer = styled.div`
+    display:flex;
+    align-items: center;
+    padding: 20px;
 
-const MessageInfo = styled.div``;
+    > img {
+        height: 50px;
+        border-radius: 8px;
+    }
+`;
+
+const MessageInfo = styled.div`
+    padding-left: 10px;
+
+    > h4 > span {
+        color: gray;
+        font-weight: 300;
+        margin-left: 4px;
+        font-size: 10px;
+    }
+`;
