c19eef8d742ffbad40cafc57edf8cea89b946b66
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-16 08:39:11 +0200

    styling the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,14 +3,34 @@ import styled from 'styled-components';
 
 const Login = () => {
     return (
-        <div>
-            
-        </div>
+        <LoginContainer>
+            <LoginInnerContainer>
+                <img src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" alt=""/>
+            </LoginInnerContainer>
+        </LoginContainer>
     );
 }
 
 export default Login;
 
 const LoginContainer = styled.div`
+    background-color: #f8f8f8;
+    height: 100vh;
+    display: grid;
+    place-items: center;
+`;
 
+const LoginInnerContainer = styled.div`
+
+    padding: 100px;
+    text-align: center;
+    background-color: white;
+    border-radius: 10px;
+    
+
+    > img {
+        object-fit:contain;
+        height: 100px;
+        margin-bottom: 40px;
+    }
 `;
