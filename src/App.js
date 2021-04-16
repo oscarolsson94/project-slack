588cb33c90e36a1eb4efa557b7b1b95ec3f85f04
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-16 09:51:53 +0200

    adding a spinner to loading component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,26 @@ import Chat from './components/Chat';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
 import Login from './components/Login';
+import Spinner from 'react-spinkit';
 
 function App() {
 
     const [user, loading] = useAuthState(auth);
+
+    if (loading) {
+        return (
+            <AppLoading>
+                <AppLoadingContent>
+                    <img src="https://i.pinimg.com/originals/6e/cc/27/6ecc279ff5f0b0863d1bfda0991de926.gif" alt="" />
+                    <Spinner
+                        name="ball-spin-fade-loader"
+                        color="purple"
+                        fadeIn="none"
+                    />
+                </AppLoadingContent>
+            </AppLoading>
+        );
+    }
 
   return (
     <div className="App">          
@@ -45,5 +61,13 @@ export default App;
 const AppBody = styled.div`
     display: flex;
     height: 100vh;
+
+`;
+
+const AppLoading = styled.div`
+
+`;
+
+const AppLoadingContent = styled.div`
 
 `;
